e0e2a0884112b4a4957e5c28bd7716b11ca4861d
GitVexy <erik.appsec@gmail.com>
2025-02-24 20:57:20 +0100

    Finished CH04

--- ch04/lc01.py
+++ ch04/lc01.py
@@ -1,2 +1,13 @@
-def selection_sort(nums):
-    pass
+def selection_sort(
+    nums: list[int]
+) -> list[int]:
+    
+    for i in range(0, len(nums)):
+        smallest_idx = i
+        for j in range(smallest_idx + 1, len(nums)):
+            if nums[j] < nums[smallest_idx]:
+                smallest_idx = j
+        print(f"{nums[i]} and {nums[smallest_idx]} swapping")
+        nums[i], nums[smallest_idx] = nums[smallest_idx], nums[i]
+
+    return nums
