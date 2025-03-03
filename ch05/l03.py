2688488c732a5fe36ff69c5f770f9dab8d71e29d
GitVexy <erik.appsec@gmail.com>
2025-03-03 20:31:58 +0100

    Damn boi

---
+++ ch05/l03.py
@@ -0,0 +1,14 @@
+def fib(
+    n: int
+) -> int:
+    
+    current, grandparent, parent = int, 0, 1
+
+    if n == 0:
+        return grandparent
+    elif n == 1:
+        return 1
+
+    for i in # ENDED HERE
+
+    return fib(n - 1) + fib(n - 2)
